b296bf55f3ea03bcdc8530c7aeeb940d5978658c
mianazan099 <103571768+mianazan099@users.noreply.github.com>
2022-11-29 12:22:56 +0500

    navbar completed

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-const menuBtn = document.querySelector(".menu-btn");
-const menuBtnImg = document.querySelector(".menu-btn img");
-const navOpen = document.body;
+const menuBtn = document.querySelector(".navbar__menu-btn");
+const menuBtnImg = document.querySelector(".navbar__menu-btn img");
+const navLink = document.querySelectorAll(".nav__link");
+const body = document.body;
 
-menuBtn.addEventListener("click", () => {
-  let open = eval(navOpen.getAttribute("data-navOpen"));
-  navOpen.setAttribute("data-navOpen", !open);
+const handleNav = () => {
+  let open = eval(body.getAttribute("data-navOpen"));
+  body.setAttribute("data-navOpen", !open);
   if (open) {
     menuBtnImg.setAttribute("src", "./images/icon-hamburger.svg");
   } else {
     menuBtnImg.setAttribute("src", "./images/icon-close.svg");
   }
+};
+
+menuBtn.addEventListener("click", handleNav);
+navLink.forEach((link) => {
+  link.addEventListener("click", handleNav);
 });
