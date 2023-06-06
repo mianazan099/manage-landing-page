f359cfe29cfde8c0f94a7310a4636372f786d931
mian azan <mianazan024@gmail.com>
2023-06-06 22:38:47 +0500

    navbar done

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -2,7 +2,35 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      lg: "54rem",
+      md: "40rem",
+    },
+    colors: {
+      blue: "hsl(228, 39%, 23%)",
+      white: "hsl(0, 0%, 100%)",
+      red: "hsl(0, 100%, 70%)",
+      gray: {
+        900: "hsl(233, 12%, 13%)",
+        700: "hsl(233, 12%, 33%)",
+        400: "hsl(230, 12%, 60%)",
+        100: "hsl(233, 12%, 70%)",
+      },
+      orange: {
+        800: "hsl(12, 88%, 59%)",
+        600: "hsl(12, 94%, 72%)",
+        400: "hsl(12, 88%, 80%)",
+        100: "hsl(13, 100%, 96%)",
+      },
+    },
+    fontFamily: {
+      serif: ["Be Vietnam Pro", "sans-serif"],
+    },
+    fontWeight: {
+      regular: 400,
+      medium: 500,
+      bold: 700,
+    },
   },
   plugins: [],
 };
